3cf45ceddeb316cdab3264dccdc1b5b968e7840d
srodiftsis <srodiftsis@gmail.com>
2023-01-24 17:43:38 +0200

    07

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Message } from './Beautiful';
 
 export class Hello extends React.Component {
     render() {
         return <div>
             <h1>Hello, world!</h1>
-            <p>What a beautiful day!</p>
+            <Message />
         </div>;
     }
 }
