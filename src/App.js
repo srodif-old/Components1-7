b982eb42d1263a4c634a979812712b0c80830dd3
srodiftsis <srodiftsis@gmail.com>
2023-01-24 17:33:59 +0200

    03

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hello, HelloWorld } from "./helloWorld";
+
+export class App extends React.Component{
+    render() {
+        return <div>
+            <Hello />
+        </div>
+    }
+}
