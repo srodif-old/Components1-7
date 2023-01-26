81b3f4fb013ceb4de4a722fd043249b16ed7f07d
srodiftsis <srodiftsis@gmail.com>
2023-01-26 12:18:03 +0200

    props01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hello } from "./helloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
     render() {
         return <div>
             <Hello />
+            <Welcome name="Stavros" />
         </div>
     }
 }
