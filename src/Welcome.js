f6e01ea72726e676cbf222085711eccb04da4c43
srodiftsis <srodiftsis@gmail.com>
2023-01-26 13:52:35 +0200

    cr01

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,10 @@ export class Welcome extends React.Component {
     render({name = "user"}) {
         return <div>
             <p>Welcome, {this.props.name}</p>
-            <Age age={this.props.age} />
+            { this.props.age > 18 ?
+                <Age age={this.props.age} />
+                : <></>
+            }
         </div>
     }
 }
