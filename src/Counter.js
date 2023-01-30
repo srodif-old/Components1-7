94ee11477b7e850913579d13e8722d3ecc762129
srodiftsis <srodiftsis@gmail.com>
2023-01-30 15:10:50 +0200

    state04

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
     state = {
@@ -18,8 +19,6 @@ export class Counter extends React.Component {
     }
 
     render() {
-        return <div>
-            <h1>{this.state.count}</h1>
-        </div>
+        return <CounterDisplay count = {this.state.count} />
     }
 }
---
+++ src/CounterDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class CounterDisplay extends React.Component {
+    render() {
+        return <div>
+            <h1>{this.props.count}</h1>
+        </div>
+    }
+}
