647dc1c652f09466d3714f8b67803834d4636ef7
srodiftsis <srodiftsis@gmail.com>
2023-01-30 15:04:25 +0200

    state01

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export class Counter extends React.Component {
+    state = {
+        count: 0
+    }
+
+    constructor(props) {
+        super(props)
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: this.state.count + 1
+                }
+            })
+        }, 1000)
+    }
+
+    render() {
+        return <div>
+            <h1>{this.state.count}</h1>
+        </div>
+    }
+}
