3c751d71fcbb1a66dbdf5a66caab16aef54b740a
srodiftsis <srodiftsis@gmail.com>
2023-01-31 14:57:35 +0200

    forms02

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,14 @@ export class Login extends React.Component {
     }
 
     handleInputChange = (event) => {
-        
+        const name = event.target.name;
+        const value = event.target.value;
+        const type = event.target.type;
+        const checked = event.target.checked;
+
+        this.setState({
+            [name] : type == "checkbox" ? checked : value
+        })
     }
     
     render() {
@@ -17,17 +24,20 @@ export class Login extends React.Component {
             <input 
                 name="username" 
                 value={this.state.username}
-                onChange={this.handleInputChange} />
+                onChange={this.handleInputChange} 
+            />
             <input 
                 name="password"
                 type="password" 
                 value={this.state.password}
-                onChange={this.handleInputChange} />
+                onChange={this.handleInputChange} 
+            />
             <input 
                 name="remember"
                 type="checkbox" 
                 checked={this.state.remember}
-                onChange={this.handleInputChange} />   
+                onChange={this.handleInputChange} 
+            />   
         </div>
         
 
