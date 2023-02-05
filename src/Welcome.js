158f10e2d39a53378b7a8380a5630b1f523ec09e
srodiftsis <srodiftsis@gmail.com>
2023-02-05 22:01:41 +0200

    styling01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Age } from './Age'
+import React from 'react';
+import { Age } from './Age';
+import './index.css';
 
 export class Welcome extends React.Component {
     render({name = "user"}) {
-        return <div>
+        return <div className='welcome'>
             <p>Welcome, {this.props.name}</p>
             { (this.props.age > 18 
                 && this.props.age < 65 
