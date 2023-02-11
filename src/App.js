b5454e8ac41465c9ec9deaa5d826c90e8ec0a09b
srodiftsis <srodiftsis@gmail.com>
2023-02-11 21:09:36 +0200

    router04

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Hello } from "./helloWorld";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
 import { ShowGithubUser } from "./ShowGihubUser";
+import { Link } from "react-router-dom";
 
 export class App extends React.Component{
     
     render() {
         return (<div>
+            <Link to="/"></Link>
+            <Link to="counter"></Link>
+            <Link to="users/:username"></Link>
             <Routes>
                 <Route path="/" element={<Welcome />} />
-                <Route path="/counter" element={<ClickCounter />} />
-                <Route path="/users/:username" element={<ShowGithubUser />} />
+                <Route path="counter" element={<ClickCounter />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
             </Routes>
         </div>)
     }
