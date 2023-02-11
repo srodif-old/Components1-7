631e80a503278fe87a255d923699659585182814
srodiftsis <srodiftsis@gmail.com>
2023-02-11 20:41:06 +0200

    router02

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Hello } from "./helloWorld";
 import { Welcome } from "./Welcome";
+import { ClickCounter } from "./ClickCounter";
 
 export class App extends React.Component{
     
     render() {
         return (<div>
             <Routes>
-                <Route path="/" element={<Welcome />}></Route>
+                <Route path="/" element={<Welcome />} />
+                <Route path="/counter" element={<ClickCounter />} />
             </Routes>
         </div>)
     }
