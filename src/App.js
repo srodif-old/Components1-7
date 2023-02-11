79f90bdb6c009df8aa96d95a572de089baeb9f20
srodiftsis <srodiftsis@gmail.com>
2023-02-11 16:48:54 +0200

    router01

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { Hello } from "./helloWorld";
-import { TodoList } from "./TodoList";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
-    renderTodos = (items, removeItem) => (
-        <ul>
-            {items.map((item, index) => (
-                <li key={index}>
-                    {item}
-                    <button type="button" onClick={() => removeItem(index)}>
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ul>
-    );
     
     render() {
-        return <div>
-            <Hello />
-            <Welcome name={<strong>Stavros</strong>} age={28} />
-
-            <TodoList render={this.renderTodos} />
-        </div>
+        return (<div>
+            <Routes>
+                <Route path="/" element={<Welcome />}></Route>
+            </Routes>
+        </div>)
     }
 }
